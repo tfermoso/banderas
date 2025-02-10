396ff559d6db234eb85d7c7b7f0ec86ec7bb1802
tfermoso <tomas.fermoso@gmail.com>
2025-02-10 19:36:16 +0100

    spiner loading

--- js/banderas.js
+++ js/banderas.js
@@ -1,16 +1,14 @@
-
-var xhr=new XMLHttpRequest();
-xhr.open("GET","https://restcountries.com/v3.1/all",true);
-xhr.onload=function(){
-    if(xhr.status==200){
-        var data=JSON.parse(xhr.responseText);
-        var banderas=document.getElementById("banderas");
-        for(var i=0;i<data.length;i++){
-            var img=document.createElement("img");
-            img.src=data[i].flags.png;
-            img.alt=data[i].name.common;
+fetch("https://restcountries.com/v3.1/all")
+    .then(response =>{
+        $("#cargando").hide();
+        return response.json()})
+    .then(data => {
+        let banderas = document.getElementById("banderas");
+        for (let i = 0; i < data.length; i++) {
+            let img = document.createElement("img");
+            img.src = data[i].flags.png;
+            img.alt = data[i].name.common;
             banderas.appendChild(img);
         }
-    }
-}
-xhr.send();
+    })
+    .catch(error => console.error(error));
